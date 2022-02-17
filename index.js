1d4d051a4383e995ad5dbdc74156899295fb107f
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2022-02-17 16:05:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ io.on('connection', (socket) => {
     socket.on('login', ({ name, room }, callback) => {
         const { user, error } = addUser(socket.id, name, room)
         if (error) return callback(error)
-        if (user.length < 3)
-        {
-            return { error: "Enter at least 3 characters" }
-        }
         socket.join(user.room)
         socket.in(room).emit('notification', { title: 'Someone\'s here', description: `${user.name} just entered the room` })
         io.in(room).emit('users', getUsers(room))
