d9656cbf4f6c2d325617fa51ca9bfcfec441cbc5
Sambhav Saxena <sambhavsaxena02@gmail.com>
2022-02-18 17:04:12 +0530

    circuit changes

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ io.on('connection', (socket) => {
         callback()
     })
 
+    socket.on('typing', data => {
+        const user = getUser(socket.id)
+        if (data.typing == true)
+            io.in(user.room).emit('display', data)
+        else
+            io.in(user.room).emit('display', data)
+    })
+
     socket.on('sendMessage', message => {
         const user = getUser(socket.id)
         io.in(user.room).emit('message', { user: user.name, text: message });
